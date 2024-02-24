d6b8ea55059ca3da79b41862dbc834e41de0f939
Tiiito21 <alejandroacba03@gmail.com>
2024-02-24 18:53:18 +0100

    Added Carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+import animations from '@midudev/tailwind-animations'
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
+  ],
 	theme: {
     extend: {
       fontFamily: {
@@ -8,5 +14,5 @@ export default {
       },
     },
   },
-	plugins: [],
+	plugins: [animations],
 }
